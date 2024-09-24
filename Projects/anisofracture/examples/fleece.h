cebe7ff29563b5f571775c1717b5fe1bcf80967f
Daniel <Daniel.A.PerezBanuelos@student.uts.edu.au>
2024-09-24 13:50:26 +1000

    Image Comparison added

--- Projects/anisofracture/examples/fleece.h
+++ Projects/anisofracture/examples/fleece.h
@@ -11,7 +11,7 @@ std::string helper_output;
 helper_output = "output/RIG_fleece_fibres";
 std::cout << "Attempt 1" << std::endl;
 sim.output_dir.path = helper_output;
-sim.end_frame = 7;
+sim.end_frame = 12;
 T frameRate = 1;
 sim.step.frame_dt = (T)1 / frameRate;
 sim.gravity = -9.81 * TV::Unit(1);
@@ -24,141 +24,116 @@ sim.dump_F_for_meshing = true;
 T particle_per_cell = 50;
 sim.rpic_damping_iteration = 0;
 
-// ****************************************************************************
-// Loop for all files
-// ****************************************************************************
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// Parameters that can be modified ////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+std::map<std::string, double> parameters;
 
-std::ifstream file;
-file.open("../../Data/TetMesh/mesh_files/fibre_direction.csv");
-std::vector<std::vector<double>> directions;
-std::string line;
+// Open the parameters file for reading
+std::ifstream paramFile("../cmaes/parameters.txt");
 
-if (!file.is_open())
-  throw std::runtime_error("Could not open file");
-
-while (std::getline(file, line)) {
-  std::stringstream s(line);
-  std::vector<double> row;
-  std::string val;
-  while (std::getline(s, val, ',')) {
-    row.push_back(std::stod(val));
-  }
-
-  directions.push_back(row);
+if (!paramFile.is_open()) {
+  std::cerr << "Error: Could not open parameters file." << std::endl;
 }
 
-file.close();
-
-for (size_t file_index = 0; file_index < 98; ++file_index) {
-
-  // Construct the file path based on the index
-  std::string meshFilePath =
-      "/Fibre_directions/Aniso_MPM/Data/TetMesh/mesh_files/file_" +
-      std::to_string(file_index + 1) + ".mesh";
-
-  FILE *file = std::fopen(meshFilePath.c_str(), "r");
-
-  if (file != nullptr) {
-    std::fclose(file);
-
-    // Get the direction corresponding to this file
-    std::vector<double> helper_fiber_vector = directions[file_index];
-
-    bool helper_anisotropic =
-        std::all_of(helper_fiber_vector.begin(), helper_fiber_vector.end(),
-                    [](int i) { return i == 0; });
-
-    TV helper_fiber(helper_fiber_vector[0], helper_fiber_vector[1],
-                    helper_fiber_vector[2]);
-
-    // Print the values to verify
-    // std::cout << "File index: " << file_index << " Helper Isotropic: " <<
-    // helper_isotropic
-    // << " Helper Fiber: [" << helper_fiber[0] << ", " << helper_fiber[1] << ",
-    // " << helper_fiber[2] << "]" << std::endl;
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /// Parameters that can be modified ////
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    std::map<std::string, double> parameters;
-
-    // Open the parameters file for reading
-    std::ifstream paramFile("../cmaes/parameters.txt");
-
-    if (!paramFile.is_open()) {
-      std::cerr << "Error: Could not open parameters file." << std::endl;
-    }
-
-    std::string line;
-    while (std::getline(paramFile, line)) {
-      size_t separatorPos = line.find(":");
-      if (separatorPos != std::string::npos) {
-        std::string paramName = line.substr(0, separatorPos);
-        double paramValue = std::stod(line.substr(separatorPos + 1));
-        parameters[paramName] = paramValue;
-      }
-    }
-
-    // Close the parameters file
-    paramFile.close();
-
-    // Access the parameters by their names
-    T Youngs = parameters["Youngs"];
-    T nu = parameters["nu"];
-    T rho = parameters["rho"];
-    T percentage = parameters["percentage"];
-    T eta = parameters["eta"];
-    double fiberstifness = parameters["fiber"];
-
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /// End ////
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    sim.cfl = 0.4;
-    T helper_alpha = 0;
-    T residual_stress = 0.01;
-
-    // std::string filename = "TetMesh/RIG_test_holes.mesh";
-    MpmParticleHandleBase<T, dim> particles_handle =
-        init_helper.sampleFromTetWildFile(meshFilePath, rho);
-    T total_volume = particles_handle.total_volume;
-    T particle_count = particles_handle.particle_range.length();
-    T per_particle_volume = total_volume / particle_count;
-    sim.dx = std::pow(particle_per_cell * per_particle_volume, (T)1 / (T)3);
-
-    /*
-    if (1) {
-      StdVector<TV> samples;
-      StdVector<Vector<int, 4>> indices;
-      std::string absolute_path = DataDir().absolutePath(meshFilePath);
-      readTetMeshTetWild(absolute_path, samples, indices);
-      sim.output_dir.createPath();
-      std::string vtk_path = DataDir().path + "/../Projects/anisofracture/" +
-                             sim.output_dir.path + "/tet2.vtk";
-      writeTetmeshVtk(vtk_path, samples, indices);
-    }
-    */
-
-    QRAnisotropic<T, dim> model(Youngs, nu, helper_anisotropic);
-    StdVector<TV> a_0;
-    StdVector<T> alphas;
-    TV a_1, a_2;
-    a_1 = helper_fiber;
-    a_1.normalize();
-    a_0.push_back(a_1);
-    alphas.push_back(helper_alpha);
-    T l0 = 0.5 * sim.dx;
-
-    T zeta = 1;
-    bool allow_damage = true;
-    // model.scaleFiberStiffness(0, 2);
-    model.scaleFiberStiffness(0, fiberstifness);
-    particles_handle.addFBasedMpmForceWithAnisotropicPhaseField(
-        a_0, alphas, percentage, l0, model, eta, zeta, allow_damage,
-        residual_stress);
-  } else {
-    std::cout << "File does not exist: " << meshFilePath << std::endl;
-    continue;
+std::string line;
+while (std::getline(paramFile, line)) {
+  size_t separatorPos = line.find(":");
+  if (separatorPos != std::string::npos) {
+    std::string paramName = line.substr(0, separatorPos);
+    double paramValue = std::stod(line.substr(separatorPos + 1));
+    parameters[paramName] = paramValue;
   }
+}
+
+// Close the parameters file
+paramFile.close();
+
+// Access the parameters by their names
+T Youngs = parameters["Youngs"];
+T nu = parameters["nu"];
+T rho = parameters["rho"];
+T percentage = parameters["percentage"];
+T eta = parameters["eta"];
+double fiberstifness = parameters["fiber"];
+
+sim.cfl = 0.4;
+T helper_alpha = 0;
+T residual_stress = 0.01;
+
+TV helper_fiber = TV(1, 1, 1); // will overwrite with radial fiber
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// File and Fibre Direction ////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Construct the file path based on the index
+std::string meshFilePath = "TetMesh/split/sample1_top.mesh";
+
+MpmParticleHandleBase<T, dim> particles_handle =
+    init_helper.sampleFromTetWildFile(meshFilePath, rho);
+T total_volume = particles_handle.total_volume;
+T particle_count = particles_handle.particle_range.length();
+T per_particle_volume = total_volume / particle_count;
+sim.dx = std::pow(particle_per_cell * per_particle_volume, (T)1 / (T)3);
+
+if (1) {
+  StdVector<TV> samples;
+  StdVector<Vector<int, 4>> indices;
+  std::string absolute_path = DataDir().absolutePath(meshFilePath);
+  readTetMeshTetWild(absolute_path, samples, indices);
+  sim.output_dir.createPath();
+  std::string vtk_path = DataDir().path + "/../Projects/anisofracture/" +
+                         sim.output_dir.path + "/tet.vtk";
+  writeTetmeshVtk(vtk_path, samples, indices);
+}
+
+StdVector<TV> node_wise_fiber;
+if (1) {
+  StdVector<TV> samples;
+  StdVector<Vector<int, 4>> indices;
+  std::string absolute_path = DataDir().absolutePath(meshFilePath);
+  readTetMeshTetWild(absolute_path, samples, indices);
+  std::function<bool(TV, int)> inflow = [](TV X, int vertex) {
+    return (X[1] > 2.15);
+  };
+  std::function<bool(TV, int)> outflow = [](TV X, int vertex) {
+    return (X[1] < 1.65);
+  };
+  StdVector<TV> tet_wise_fiber;
+  fiberGen(samples, indices, inflow, outflow, tet_wise_fiber, node_wise_fiber);
+}
+
+QRAnisotropic<T, dim> model(Youngs, nu);
+StdVector<TV> a_0;
+StdVector<T> alphas;
+TV a_1, a_2;
+a_1 = helper_fiber;
+a_1.normalize();
+a_0.push_back(a_1);
+alphas.push_back(helper_alpha);
+T l0 = 0.5 * sim.dx;
+
+T zeta = 1;
+bool allow_damage = true;
+model.scaleFiberStiffness(0, fiberstifness);
+particles_handle.transform([&](int index, Ref<T> mass, TV &X, TV &V) {
+  X += TV(2, 2, 2);
+});
+particles_handle.addFBasedMpmForceWithAnisotropicPhaseField(a_0, alphas,
+                                                            percentage, l0,
+                                                            model, eta, zeta,
+                                                            allow_damage,
+                                                            residual_stress);
+
+int i = 0;
+for (auto iter = particles_handle.particles.subsetIter(
+         DisjointRanges{particles_handle.particle_range}, F_name<T, dim>());
+     iter; ++iter) {
+  auto &F = iter.template get<0>();
+  TV fiber = node_wise_fiber[i++];
+  StdVector<TV> a0;
+  a0.emplace_back(fiber);
+  F = particles_handle.initializeRotatedFHelper(a0);
 }
 
 // ****************************************************************************
@@ -203,7 +178,6 @@ auto left1Transform = [](T time, AnalyticCollisionObject<T, dim> &object) {
 // 2
 /////////
 {
-
   CappedCylinder<T, dim> cylinder12(
       sphere_radius, 0.5, Vector<T, 4>(1, 0, 0, 0), TV(2.017, 1.825, 1.975));
   HalfSpace<T, dim> board1(TV(0, 1, 0), TV(0, 1, 0));
@@ -218,7 +192,6 @@ auto left1Transform = [](T time, AnalyticCollisionObject<T, dim> &object) {
 // 3
 /////////
 {
-
   CappedCylinder<T, dim> cylinder13(
       sphere_radius, 0.5, Vector<T, 4>(1, 0, 0, 0), TV(2.065, 1.825, 1.983));
   HalfSpace<T, dim> board1(TV(0, 1, 0), TV(0, 1, 0));
@@ -241,7 +214,6 @@ auto right1Transform = [](T time, AnalyticCollisionObject<T, dim> &object) {
 // 1
 /////////
 {
-
   CappedCylinder<T, dim> cylinder21(
       sphere_radius, 0.5, Vector<T, 4>(1, 0, 0, 0), TV(1.972, 1.825, 2.01));
   HalfSpace<T, dim> board2(TV(0, 1, 0), TV(0, 1, 0));
@@ -255,7 +227,6 @@ auto right1Transform = [](T time, AnalyticCollisionObject<T, dim> &object) {
 // 2
 /////////
 {
-
   CappedCylinder<T, dim> cylinder22(
       sphere_radius, 0.5, Vector<T, 4>(1, 0, 0, 0), TV(2.015, 1.825, 2.01));
   HalfSpace<T, dim> board2(TV(0, 1, 0), TV(0, 1, 0));
@@ -269,7 +240,6 @@ auto right1Transform = [](T time, AnalyticCollisionObject<T, dim> &object) {
 // 3
 /////////
 {
-
   CappedCylinder<T, dim> cylinder23(sphere_radius, 0.5,
                                     Vector<T, 4>(1, 0, 0, 0),
                                     TV(2.065, 1.825, 2.015)); // End frame 2.019
--- Projects/anisofracture/examples/pork.h
+++ Projects/anisofracture/examples/pork.h
@@ -5,11 +5,11 @@
 
 bool orthotropic = Python::orthotropic;
 
-sim.end_frame = 72; // 72
-T frameRate = 24;   // 24
+sim.end_frame = 384; // 72
+T frameRate = 24;    // 24
 sim.step.frame_dt = (T)1 / frameRate;
 sim.dx = 0.01;                              // use 0.0025 for high res
-sim.gravity = -.00003 * TV::Unit(1);        // no gravity
+sim.gravity = -3 * TV::Unit(1);             // no gravity
 sim.step.max_dt = /*1e-3*/ /*3e-4*/ 2.5e-4; // with test params can use 8e-5
 sim.newton.max_iterations = 5;
 sim.newton.tolerance = 1e-3;
@@ -34,7 +34,7 @@ T rho = 2;
 QRStableNeoHookean<T, dim> model(Youngs, nu);
 
 // sample particles from a .mesh file
-std::string filename = "TetMesh/fleece_2_500k.mesh"; // 50k is plenty for nice
+std::string filename = "TetMesh/porkBelly500k.mesh"; // 50k is plenty for nice
                                                      // dynamics and no debris
 MpmParticleHandleBase<T, dim> particles_handle =
     init_helper.sampleFromTetWildFile(filename, rho);
@@ -51,42 +51,34 @@ sim.dx = std::pow(particle_per_cell * per_particle_volume, (T)1 / (T)3);
 StdVector<TV> a_0;
 StdVector<T> alphas;
 TV a_1, a_2;
-
-a_1[0] = 0;
-a_1[1] = 0;
-a_1[2] = 1; // Fibres going straight up
-a_1.normalize();
-a_0.push_back(a_1);
-alphas.push_back(-1);
-
-// if (orthotropic) { // orthotropic
-//   a_1[0] = 1;
-//   a_1[1] = 0;
-//   a_1[2] = 0;
-//   a_1.normalize();
-//   a_0.push_back(a_1);
-//   alphas.push_back(-1);
-//   a_2[0] = 0;
-//   a_2[1] = 0;
-//   a_2[2] = 1;
-//   a_2.normalize();
-//   a_0.push_back(a_2);
-//   alphas.push_back(-1);
-// } else if (!Python::isotropic) { // transverse isotropic
-//   a_1[0] = 1;
-//   a_1[1] = 0; // 45 deg in XZ plane
-//   a_1[2] = 1;
-//   a_1.normalize();
-//   a_0.push_back(a_1);
-//   alphas.push_back(-1);
-// } else { // isotropic
-//   a_1[0] = 1;
-//   a_1[1] = 0;
-//   a_1[2] = 0;
-//   a_1.normalize();
-//   a_0.push_back(a_1);
-//   alphas.push_back(0);
-// }
+if (orthotropic) { // orthotropic
+  a_1[0] = 1;
+  a_1[1] = 0;
+  a_1[2] = 0;
+  a_1.normalize();
+  a_0.push_back(a_1);
+  alphas.push_back(-1);
+  a_2[0] = 0;
+  a_2[1] = 0;
+  a_2[2] = 1;
+  a_2.normalize();
+  a_0.push_back(a_2);
+  alphas.push_back(-1);
+} else if (!Python::isotropic) { // transverse isotropic
+  a_1[0] = 1;
+  a_1[1] = 0; // 45 deg in XZ plane
+  a_1[2] = 1;
+  a_1.normalize();
+  a_0.push_back(a_1);
+  alphas.push_back(-1);
+} else { // isotropic
+  a_1[0] = 1;
+  a_1[1] = 0;
+  a_1[2] = 0;
+  a_1.normalize();
+  a_0.push_back(a_1);
+  alphas.push_back(0);
+}
 
 T percentage = Python::percent;
 T l0 = 0.5 * sim.dx;
@@ -115,34 +107,27 @@ std::string scale_str = std::to_string(Python::fiberScale);
 scale_str.erase(scale_str.find_last_not_of('0') + 1, std::string::npos);
 std::cout << "FiberScale = " << Python::fiberScale << std::endl;
 
-// if (Python::isotropic) {
-//   std::string path("output/3D_Fleece/3D_Pork_Isotropic_Youngs" + E_str +
-//                    "_percent" + percent_str + "_eta" + eta_str + "_dx" +
-//                    dx_str + "_fiberScale" + scale_str);
-//   sim.output_dir.path = path;
-// } else if (orthotropic) {
-//   std::string path("output/3D_Fleece/3D_Pork_Orthotropic_Youngs" + E_str +
-//                    "_percent" + percent_str + "_eta" + eta_str + "_dx" +
-//                    dx_str + "_fiberScale" + scale_str);
-//   sim.output_dir.path = path;
-//   model.setExtraFiberStiffness(
-//       0, Python::fiberScale); // only scale elasticity if anisotropic!
-//   model.setExtraFiberStiffness(1, Python::fiberScale);
-// } else {
-//   std::string path("output/3D_Fleece/3D_Pork_TransverseIsotropic_Youngs" +
-//   E_str +
-//                    "_percent" + percent_str + "_eta" + eta_str + "_dx" +
-//                    dx_str + "_fiberScale" + scale_str);
-//   sim.output_dir.path = path;
-//   model.setExtraFiberStiffness(
-//       0, Python::fiberScale); // only scale elasticity if anisotropic!
-// }
-
-std::string path("output/3D_Fleece/Fleece_Orthotropic");
-sim.output_dir.path = path;
-model.setExtraFiberStiffness(
-    0, Python::fiberScale); // only scale elasticity if anisotropic!
-model.setExtraFiberStiffness(1, Python::fiberScale);
+if (Python::isotropic) {
+  std::string path("output/3D_Pork/3D_Pork_Isotropic_Youngs" + E_str +
+                   "_percent" + percent_str + "_eta" + eta_str + "_dx" +
+                   dx_str + "_fiberScale" + scale_str);
+  sim.output_dir.path = path;
+} else if (orthotropic) {
+  std::string path("output/3D_Pork/3D_Pork_Orthotropic_Youngs" + E_str +
+                   "_percent" + percent_str + "_eta" + eta_str + "_dx" +
+                   dx_str + "_fiberScale" + scale_str);
+  sim.output_dir.path = path;
+  model.setExtraFiberStiffness(
+      0, Python::fiberScale); // only scale elasticity if anisotropic!
+  model.setExtraFiberStiffness(1, Python::fiberScale);
+} else {
+  std::string path("output/3D_Pork/3D_Pork_TransverseIsotropic_Youngs" + E_str +
+                   "_percent" + percent_str + "_eta" + eta_str + "_dx" +
+                   dx_str + "_fiberScale" + scale_str);
+  sim.output_dir.path = path;
+  model.setExtraFiberStiffness(
+      0, Python::fiberScale); // only scale elasticity if anisotropic!
+}
 
 // dump a vtk file in the output folder for mpmmeshing
 if (1) {
@@ -164,7 +149,7 @@ T materialToHold = 0.03;
 TV boxMin(2 - (xSize / 2.0), 2 - (ySize / 2.0) - materialToHold,
           2 - (zSize /
                2.0)); // needs extra material at bottom to hold onto for mode 1
-TV boxMax(2 + (xSize / 2.0), 2 + (ySize / 2.0), 2.1 + (zSize / 2.0));
+TV boxMax(2 + (xSize / 2.0), 2 + (ySize / 2.0), 2 + (zSize / 2.0));
 // AxisAlignedAnalyticBox<T, dim> boxLevelSet(boxMin, boxMax);
 
 // Sample particles in the desired level set
@@ -191,68 +176,34 @@ init_helper.addAnalyticCollisionObject(holderObj);
 // Sphere puller
 // TV sphereCenter(1.8, 2.1, 1.8);
 // T sphereRadius = 0.05;
-// TV sphereCenter(1.865, 2.198, 1.865);
-
-// New sphere parameters
-TV leftSphereCenter(2, 2, 1.95);
-TV rightSphereCenter(2, 2, 2.05);
+TV sphereCenter(1.865, 2.198, 1.865);
 T sphereRadius = 0.05;
 
-auto leftSphereTransform = [](T time, AnalyticCollisionObject<T, dim> &object) {
-  T speed = 0.1;
-  TV translation_velocity(-1 * speed, 0, 0);
-  TV translation(-1 * speed * time, 0,
-                 0); // multiply each velocity by dt to get dx!
-  object.setTranslation(translation, translation_velocity);
+T startTime = 6; // wait some time to let it rest
+
+auto sphereTransform = [=](T time, AnalyticCollisionObject<T, dim> &object) {
+  T speed = 0.05;
+  T xVel = speed;
+  T yVel = speed;
+  T zVel = speed;
+
+  if (time < startTime) { // don't move yet
+    TV translation = TV(10, 10, 10);
+    TV translation_velocity(0, 0, 0);
+    object.setTranslation(translation, translation_velocity);
+  } else { // normal translation
+    TV translation = TV(xVel * (time - startTime), yVel * (time - startTime),
+                        zVel * (time - startTime));
+    TV translation_velocity(xVel, yVel, zVel);
+    object.setTranslation(translation, translation_velocity);
+  }
 };
 
-auto rightSphereTransform = [](T time,
-                               AnalyticCollisionObject<T, dim> &object) {
-  T speed = 0.1;
-  TV translation_velocity(1 * speed, 0, 0);
-  TV translation(1 * speed * time, 0,
-                 0); // multiply each velocity by dt to get dx!
-  object.setTranslation(translation, translation_velocity);
-};
-
-Sphere<T, dim> leftSphere(leftSphereCenter, sphereRadius);
-Sphere<T, dim> rightSphere(rightSphereCenter, sphereRadius);
-
+Sphere<T, dim> sphere(sphereCenter, sphereRadius);
 AnalyticCollisionObject<T, dim>
-    leftObject(leftSphereTransform, leftSphere,
-               AnalyticCollisionObject<T, dim>::STICKY);
-AnalyticCollisionObject<T, dim>
-    rightObject(rightSphereTransform, rightSphere,
-                AnalyticCollisionObject<T, dim>::STICKY);
-
-init_helper.addAnalyticCollisionObject(leftObject);
-init_helper.addAnalyticCollisionObject(rightObject);
-
-// T startTime = 6; // wait some time to let it rest
-
-// auto sphereTransform = [=](T time, AnalyticCollisionObject<T, dim> &object) {
-//   T speed = 0.05;
-//   T xVel = speed;
-//   T yVel = speed;
-//   T zVel = speed;
-
-//   if (time < startTime) { // don't move yet
-//     TV translation = TV(10, 10, 10);
-//     TV translation_velocity(0, 0, 0);
-//     object.setTranslation(translation, translation_velocity);
-//   } else { // normal translation
-//     TV translation = TV(xVel * (time - startTime), yVel * (time - startTime),
-//                         zVel * (time - startTime));
-//     TV translation_velocity(xVel, yVel, zVel);
-//     object.setTranslation(translation, translation_velocity);
-//   }
-// };
-
-// Sphere<T, dim> sphere(sphereCenter1, sphereRadius);
-// AnalyticCollisionObject<T, dim>
-//     sphereObject(sphereTransform, sphere,
-//                  AnalyticCollisionObject<T, dim>::STICKY);
-// init_helper.addAnalyticCollisionObject(sphereObject);
+    sphereObject(sphereTransform, sphere,
+                 AnalyticCollisionObject<T, dim>::STICKY);
+init_helper.addAnalyticCollisionObject(sphereObject);
 
 // Holder Wall
 // T wallX = 2 + 0.37 - (0.5 / 2.0);
